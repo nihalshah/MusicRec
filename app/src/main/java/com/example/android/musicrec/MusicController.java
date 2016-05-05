44966982b3ee234997558ea621a9891cd2b03cb3
Nihal <nsnihalshah@gmail.com>
2016-05-05 15:43:07 -0500

    First Commit

---
+++ app/src/main/java/com/example/android/musicrec/MusicController.java
@@ -0,0 +1,16 @@
+package com.example.android.musicrec;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by Nihal on 5/4/16.
+ */
+public class MusicController extends MediaController {
+
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+}
